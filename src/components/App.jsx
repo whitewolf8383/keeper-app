eddcff1aabdbecf43a6ff36047939e241af52ccc
whitewolf8383 <kandt.vandf@gmail.com>
2021-09-03 22:36:36 -0600

    Cmpleted functionality, finish css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,41 @@
-import React from "react";
-
-// Data
-import notes from "../notes";
+import React, { useState } from "react";
 
 // Components
 import Header from "./Header";
 import Note from "./Note";
+import CreateArea from "./CreateArea";
 import Footer from "./Footer";
 
-function App(){
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note) {
+    setNotes(prevNotes => {
+      return [...prevNotes, note];
+    });
+  };
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return(
     <div className="app-div">
       <Header />
-      {notes.map((notes) => {
+      <CreateArea
+        onAdd={addNote}
+      />
+      {notes.map((noteItem, index) => {
         return <Note 
-          key = {notes.id}
-          title = {notes.title}
-          message = {notes.message}
+          key={index}
+          id={index}
+          title={noteItem.title} 
+          content={noteItem.content} 
+          onDelete={deleteNote} 
         />
       })}
       <Footer />
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 function Note(props) {
+  function deleteNote() {
+    props.onDelete(props.id);
+  }
+
   return(
     <div className="note-div">
       <h1>{props.title}</h1>
       <p>{props.message}</p>
+      <button onClick={deleteNote}>DELETE</button>
     </div>
   );
 };
